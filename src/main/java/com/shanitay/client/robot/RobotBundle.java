228e2dfa562206af7eaa6ffdc92a3c3b445af4f5
ItaySabato@gmail.com <ItaySabato@gmail.com@507026f8-b3a8-0d27-c89a-a5f590b04826>
2012-05-19 19:19:38 +0000

    new robot

--- src/main/java/com/shanitay/client/robot/RobotBundle.java
+++ src/main/java/com/shanitay/client/robot/RobotBundle.java
@@ -9,7 +9,7 @@ import org.vectomatic.dom.svg.ui.SVGResource;
  * Time: 04:01 <br/>
  */
 interface RobotBundle extends ClientBundle {
-    @Source("robot.svg")
+    @Source("robot2.svg")
     @SVGResource.Validated(validated = false)
     SVGResource mainSvg();
 }
