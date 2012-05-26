35b4e593499c6fa05323127c90e380ffcf0a81e3
ItaySabato@gmail.com <ItaySabato@gmail.com@507026f8-b3a8-0d27-c89a-a5f590b04826>
2012-05-26 17:24:40 +0000

    app engine

---
+++ trunk/deploy_app_engine.cmd
@@ -0,0 +1,2 @@
+cd "C:\Program Files (x86)\appengine-java-sdk-1.6.6\bin"
+echo akua2tj, | appcfg.cmd update C:\Users\itay\IdeaProjects\shanitay\target\shanitay-1.0-SNAPSHOT -e itaysabato@gmail.com --passin
