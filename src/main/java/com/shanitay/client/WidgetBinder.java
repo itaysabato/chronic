ae1a4931444cb6798b86e642e2edd460c015d31e
ItaySabato@gmail.com <ItaySabato@gmail.com@507026f8-b3a8-0d27-c89a-a5f590b04826>
2012-06-13 17:16:38 +0000

    straitening of a line.

---
+++ src/main/java/com/shanitay/client/WidgetBinder.java
@@ -0,0 +1,12 @@
+package com.shanitay.client;
+
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Created By: Itay Sabato<br/>
+ * Date: 13/06/12 <br/>
+ * Time: 19:09 <br/>
+ */
+public interface WidgetBinder {
+    Widget initWidget();
+}
