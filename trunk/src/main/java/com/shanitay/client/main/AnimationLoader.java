df9e7c733153d9ac8901094066354cbda3ed57b5
ItaySabato@gmail.com <ItaySabato@gmail.com@507026f8-b3a8-0d27-c89a-a5f590b04826>
2012-06-15 21:17:20 +0000

    start binding some widgets!

--- trunk/src/main/java/com/shanitay/client/main/AnimationLoader.java
+++ trunk/src/main/java/com/shanitay/client/main/AnimationLoader.java
@@ -1,147 +1,121 @@
 package com.shanitay.client.main;
 
+import com.shanitay.client.utils.SvgToyAnimation;
+import com.shanitay.client.utils.Toy;
+import org.vectomatic.dom.svg.OMSVGAnimationElement;
+
 /**
  * Created By: Itay Sabato<br/>
  * Date: 17/05/12 <br/>
  * Time: 20:05 <br/>
  */
 class AnimationLoader {
-//
-//    private static final int BG_INTERVAL = 125;
-//    private static final int EXPLODE_DUR = 250;
-//    private static final int TOOTH_DURATION = 250;
-//    public static final int ELECTRIC_DURATION = 125;
-//    private static final int RIGHT_EAR_DURATION = 500;
-//    private static final int COOP_CENTER_DURATION = 250;
-//
     private final ElementLoader elementLoader;
-//    final Toy.Animation tooth1;
-//    final Toy.Animation tooth2;
-//    final Toy.Animation tooth3;
-//    final Toy.Animation tooth4;
-//    final Toy.Animation tooth5;
-//    final Toy.Animation tooth6;
-//    final Toy.Animation electric;
-//    final Toy.Animation earLeft;
-//    final Toy.Animation earRight;
-//    final Toy.Animation coopCenter;
-//    final Toy.Animation coopBg;
-//    final Toy.Animation step1Explode;
-//    final Toy.Animation step2Explode;
-//    final Toy.Animation step3Explode;
-//    final Toy.Animation step4Explode;
-//    final Toy.Animation step5Explode;
-//    final Toy.Animation step6Explode;
-//    final Toy.Animation step7Explode;
-//    final Toy.Animation pumpColors;
-//    final Toy.Animation pumpHouse;
-//    final Toy.Animation bgPump;
-//    final Toy.Animation leftBraw;
-//    final Toy.Animation rightBraw;
-//    final Toy.Animation mot;
-//    final Toy.Animation openMouth;
-//    final Toy.Animation closeMouth;
-//
-//
+
+    final Toy.Animation slide;
+    final Toy.Animation watchRight;
+    final Toy.Animation igulSlide;
+    final Toy.Animation orangeCube;
+    final Toy.Animation blueCube;
+    final Toy.Animation blackCube;
+    final Toy.Animation yellowMiddleCube;
+    final Toy.Animation house;
+    final Toy.Animation blueTriangle;
+    final Toy.Animation redLine;
+    final Toy.Animation yellowLineDown;
+    final Toy.Animation greenLineDown;
+    final Toy.Animation pinkButton;
+    final Toy.Animation redButton;
+    final Toy.Animation igulColor;
+    final Toy.Animation pinkLine;
+
     public AnimationLoader(ElementLoader elementLoader) {
         this.elementLoader = elementLoader;
+
+        slide = createSlide();
+        watchRight = createWatchRight();
+        igulSlide = createIgulSlide();
+        orangeCube = createOrangeCube();
+        blueCube = createBlueCube();
+        blackCube = createBlackCube();
+        yellowMiddleCube = createYellowMiddleCube();
+        house = createHouse();
+        blueTriangle = createBlueTriangle();
+        redLine = createRedLine();
+        yellowLineDown = createYellowLineDown();
+        greenLineDown = createGreenLineDown();
+        pinkButton = createPinkButton();
+        redButton = createRedButton();
+        igulColor = createIgulColor();
+        pinkLine = createPinkLine();
     }
-//        this.elementLoader = elementLoader;
-//
-//        tooth1 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth1);
-//        tooth2 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth2);
-//        tooth3 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth3);
-//        tooth4 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth4);
-//        tooth5 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth5);
-//        tooth6 = new PeekabooToyAnimation(AnimatorImpls.DISAPPEAR, TOOTH_DURATION, elementLoader.tooth6);
-//
-//        electric = createElectric();
-//
-//        final FillColorAnimator earsAnimator = new FillColorAnimator("#1F9C8D", "#EAE984");
-//        earRight = new PeekabooToyAnimation(earsAnimator, RIGHT_EAR_DURATION, elementLoader.earRight);
-//
-//        earLeft = createLeftEar(elementLoader);
-//
-//        final FillColorAnimator coopCenterAnimator = new FillColorAnimator("#173434", "#EAE984");
-//        coopCenter = new PeekabooToyAnimation(coopCenterAnimator, COOP_CENTER_DURATION, elementLoader.coopButtonCenter);
-//        coopBg = new DiscoToyAnimation(true, AnimatorImpls.APPEAR, BG_INTERVAL, elementLoader.bgCoop1, elementLoader.bgCoop2, elementLoader.bgCoop3);
-//
-//        step1Explode = createExplodeAnimation(elementLoader.step1, elementLoader.step1Rect);
-//        step2Explode = createExplodeAnimation(elementLoader.step2, elementLoader.step2Rect);
-//        step3Explode = createExplodeAnimation(elementLoader.step3, elementLoader.step3Rect);
-//        step4Explode = createExplodeAnimation(elementLoader.step4, elementLoader.step4Rect);
-//        step5Explode = createExplodeAnimation(elementLoader.step5, elementLoader.step5Rect);
-//        step6Explode = createExplodeAnimation(elementLoader.step6, elementLoader.step6Rect);
-//        step7Explode = createExplodeAnimation(elementLoader.step7, elementLoader.step7Rect);
-//
-//        pumpColors = new DiscoToyAnimation(true, AnimatorImpls.DISAPPEAR, BG_INTERVAL, elementLoader.pumpColors3, elementLoader.pumpColors2);
-//        pumpHouse = new MovingToyAnimation(Utils.TIME_UNIT, 6, new MovementEquation(0, 20f / 41f), MovementEquation.STILL, elementLoader.pumpHouse, true);
-//        bgPump = new SequenceToyAnimation(
-//                4*BG_INTERVAL,
-//                createScheduledBgPump(elementLoader.bgPump1, 0),
-//                createScheduledBgPump(elementLoader.bgPump2, 1),
-//                createScheduledBgPump(elementLoader.bgPump3, 2),
-//                createScheduledBgPump(elementLoader.bgPump4, 3));
-//
-//        leftBraw = new SvgToyAnimation(elementLoader.leftBrawRise);
-//        rightBraw = new SvgToyAnimation(elementLoader.rightBrawRise);
-//
-//        FillColorAnimator fillColorAnimator = new FillColorAnimator("#EE8F78", "#EAE984");
-//        mot = new PeekabooToyAnimation(fillColorAnimator, 250, elementLoader.mot);
-//        openMouth = new SvgToyAnimation(elementLoader.openMouthAnimation);
-//        closeMouth = new SvgToyAnimation(elementLoader.closeMouthAnimation);
-//    }
-//
-//    private Toy.Animation createExplodeAnimation(OMSVGGElement step, OMSVGRectElement stepRect) {
-//        StateChangeAnimator stepExplodeAnimator = createStepExplodeAnimator(stepRect);
-//        return new PeekabooToyAnimation(stepExplodeAnimator, EXPLODE_DUR, step);
-//    }
-//
-//    private ScheduledAnimation createScheduledBgPump(OMSVGGElement bgPump, int i) {
-//        final PeekabooToyAnimation peekabooToyAnimation = new PeekabooToyAnimation(AnimatorImpls.APPEAR, BG_INTERVAL, bgPump);
-//        return new ScheduledAnimation(peekabooToyAnimation, i*BG_INTERVAL);
-//    }
-//
-//    private Toy.Animation createLeftEar(ElementLoader elementLoader) {
-//        return new SvgToyAnimation(elementLoader.handleMove1);
-//    }
-//
-//    private Toy.Animation createElectric() {
-//
-//        PeekabooToyAnimation lightningAnimation = new PeekabooToyAnimation(AnimatorImpls.APPEAR, ELECTRIC_DURATION, elementLoader.lightning);
-//        ScheduledAnimation scheduledLightning1 = getScheduledAnimation(0, lightningAnimation);
-//        ScheduledAnimation scheduledLightning2 = getScheduledAnimation(2 * ELECTRIC_DURATION, lightningAnimation);
-//
-//        PeekabooToyAnimation whiteAnimation = new PeekabooToyAnimation(AnimatorImpls.APPEAR, 7 * ELECTRIC_DURATION, elementLoader.electricWhite);
-//        final ScheduledAnimation scheduledWhite1 = getScheduledAnimation(3 * ELECTRIC_DURATION, whiteAnimation);
-//
-//        final PeekabooToyAnimation electricBlackAnimation = new PeekabooToyAnimation(AnimatorImpls.APPEAR, ELECTRIC_DURATION, elementLoader.electricBlack);
-//        final ScheduledAnimation scheduledBlack1 = getScheduledAnimation(4 * ELECTRIC_DURATION, electricBlackAnimation);
-//        final ScheduledAnimation scheduledBlack2 = getScheduledAnimation((6 * ELECTRIC_DURATION), electricBlackAnimation);
-//        final ScheduledAnimation scheduledBlack3 = getScheduledAnimation((8 * ELECTRIC_DURATION), electricBlackAnimation);
-//
-//        return new SequenceToyAnimation(scheduledLightning1, scheduledLightning2, scheduledWhite1, scheduledBlack1, scheduledBlack2, scheduledBlack3);
-//    }
-//
-//    private ScheduledAnimation getScheduledAnimation(int startTimeMillis, PeekabooToyAnimation peekabooToyAnimation) {
-//        return new ScheduledAnimation(peekabooToyAnimation, startTimeMillis);
-//    }
-//
-//    private StateChangeAnimator createStepExplodeAnimator(final OMSVGRectElement stepRect){
-//        return new StateChangeAnimator() {
-//            public void inAnimation(OMSVGElement target) {
-//                stepRect.getX().getBaseVal().setValue(1078.282f);
-//                stepRect.getY().getBaseVal().setValue(657.246f);
-//                stepRect.getWidth().getBaseVal().setValue(178.513f);
-//                stepRect.getHeight().getBaseVal().setValue(99.348f);
-//            }
-//
-//            public void offAnimation(OMSVGElement target) {
-//                stepRect.getX().getBaseVal().setValue(1108.282f);
-//                stepRect.getY().getBaseVal().setValue(687.246f);
-//                stepRect.getWidth().getBaseVal().setValue(118.513f);
-//                stepRect.getHeight().getBaseVal().setValue(39.348f);
-//            }
-//        };
-//    }
+
+    private Toy.Animation createSlide() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createWatchRight() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createIgulSlide() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createOrangeCube() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createBlueCube() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createBlackCube() {
+        return getAnimation("moveBlackCube");
+    }
+
+    private Toy.Animation getAnimation(String elementId) {
+        OMSVGAnimationElement animationElement = elementLoader.getAnimation(elementId);
+        return new SvgToyAnimation(animationElement);
+    }
+
+    private Toy.Animation createYellowMiddleCube() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createHouse() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createBlueTriangle() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createRedLine() {
+        return getAnimation("redLineMove");
+    }
+
+    private Toy.Animation createYellowLineDown() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createGreenLineDown() {
+        return getAnimation("greenLineMove");
+    }
+
+    private Toy.Animation createPinkButton() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createRedButton() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createIgulColor() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private Toy.Animation createPinkLine() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
 }
--- trunk/src/main/java/com/shanitay/client/main/MainWidgetBinder.java
+++ trunk/src/main/java/com/shanitay/client/main/MainWidgetBinder.java
@@ -2,6 +2,7 @@ package com.shanitay.client.main;
 
 import com.google.gwt.core.client.GWT;
 import com.shanitay.client.AbstractSvgWidgetBinder;
+import com.shanitay.client.utils.Utils;
 import org.vectomatic.dom.svg.OMSVGSVGElement;
 import org.vectomatic.dom.svg.ui.SVGResource;
 
@@ -12,145 +13,31 @@ import org.vectomatic.dom.svg.ui.SVGResource;
  */
 public class MainWidgetBinder extends AbstractSvgWidgetBinder {
 
-    protected OMSVGSVGElement bindWidgets() {
-        MainBundle mainBundle = GWT.create(MainBundle.class);
-        final SVGResource svgResource = mainBundle.mainSvg();
-        final OMSVGSVGElement svgElement = svgResource.getSvg();
+    private SoundLoader soundLoader;
+    private ElementLoader elementLoader;
+    private AnimationLoader animationLoader;
 
-        final SoundLoader soundLoader = new SoundLoader();
-        final ElementLoader elementLoader = new ElementLoader(svgElement);
-        final AnimationLoader animationLoader = new AnimationLoader(elementLoader);
+    protected OMSVGSVGElement bindWidgets() {
+        final OMSVGSVGElement svgElement = getSvg();
+        soundLoader = new SoundLoader();
+        elementLoader = new ElementLoader(svgElement);
+        animationLoader = new AnimationLoader(elementLoader);
+
+        bindBlackCube();
+        Utils.attachToy(elementLoader.greenLineDown, soundLoader.greenLineDown, false, animationLoader.greenLineDown);
+        Utils.attachToy(elementLoader.redLine, soundLoader.redLine, false, animationLoader.redLine);
+
 
         return svgElement;
     }
-//        initRecorder(svgElement);
-//
-//        Utils.attachToy(elementLoader.tooth1, soundLoader.tooth1, false, animationLoader.tooth1);
-//        Utils.attachToy(elementLoader.tooth2, soundLoader.tooth2, false, animationLoader.tooth2);
-//        Utils.attachToy(elementLoader.tooth3, soundLoader.tooth3, false, animationLoader.tooth3);
-//        Utils.attachToy(elementLoader.tooth4, soundLoader.tooth4, false, animationLoader.tooth4);
-//        Utils.attachToy(elementLoader.tooth5, soundLoader.tooth5, false, animationLoader.tooth5);
-//        Utils.attachToy(elementLoader.tooth6, soundLoader.tooth6, false, animationLoader.tooth6);
-//
-//        Utils.addHandler(elementLoader.lightningButton, new Utils.SomeHandler() {
-//            public void handle() {
-//                Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
-//                    public boolean execute() {
-//                        Utils.hide(elementLoader.eyes);
-//                        Utils.show(elementLoader.surprised);
-//                        Utils.hide(elementLoader.glasses);
-//                        return false;
-//                    }
-//                }, 4*AnimationLoader.ELECTRIC_DURATION);
-//            }
-//        });
-//        Utils.attachToy(elementLoader.lightningButton, soundLoader.electric, false, animationLoader.electric);
-//
-//        Utils.attachToy(elementLoader.earLeft, soundLoader.earLeft, false, animationLoader.earLeft);
-//        Utils.attachToy(elementLoader.earRight, soundLoader.earRight, false, animationLoader.earRight);
-//
-//        Utils.attachToy(elementLoader.eyeBrawLeft, soundLoader.leftBraw, false, animationLoader.leftBraw);
-//        Utils.attachToy(elementLoader.eyeBrawRight, soundLoader.rightBraw, false, animationLoader.rightBraw);
-//
-//        Utils.attachToy(elementLoader.step1, soundLoader.step1, false, animationLoader.step1Explode);
-//        Utils.attachToy(elementLoader.step2, soundLoader.step2, false, animationLoader.step2Explode);
-//        Utils.attachToy(elementLoader.step3, soundLoader.step3, false, animationLoader.step3Explode);
-//        Utils.attachToy(elementLoader.step4, soundLoader.step4, false, animationLoader.step4Explode);
-//        Utils.attachToy(elementLoader.step5, soundLoader.step5, false, animationLoader.step5Explode);
-//        Utils.attachToy(elementLoader.step6, soundLoader.step6, false, animationLoader.step6Explode);
-//        Utils.attachToy(elementLoader.step7, soundLoader.step7, false, animationLoader.step7Explode);
-//
-//        Utils.attachToy(elementLoader.coopButton, soundLoader.coop, true, animationLoader.coopBg);
-//        Utils.addHandler(elementLoader.coopButton, new Utils.SomeHandler() {
-//            public void handle() {
-//                animationLoader.coopCenter.play();
-//            }
-//        });
-//
-//        Utils.attachToy(
-//                elementLoader.pumpHouse,
-//                soundLoader.pumpHouse,
-//                true,
-//                animationLoader.pumpColors,
-//                animationLoader.bgPump,
-//                animationLoader.pumpHouse);
-//
-//        Toy.Animation noseAnimation = new HandlerToyAnimation(new Utils.SomeHandler() {
-//            public void handle() {
-//                String display = elementLoader.surprised.getStyle().getDisplay();
-//                if (display.equalsIgnoreCase("none")) {
-//                    soundLoader.nose.play();
-//                    Utils.show(elementLoader.surprised);
-//                    Utils.hide(elementLoader.eyes);
-//                }
-//                else {
-//                    Utils.hide(elementLoader.surprised);
-//                    Utils.show(elementLoader.eyes);
-//                }
-//            }
-//        });
-//
-//        Utils.attachToy(elementLoader.nose, Utils.getNullSound(), false, noseAnimation);
-//
-//        createGlassesButton(soundLoader, elementLoader, elementLoader.teethDownOpen);
-//
-//        HandlerToyAnimation mouthOpenAnimation = new HandlerToyAnimation(new Utils.SomeHandler() {
-//            public void handle() {
-//                if (isMouthOpen(elementLoader)) {
-//                    animationLoader.closeMouth.play();
-//                }
-//                else {
-//                    animationLoader.openMouth.play();
-//                }
-//            }
-//        });
-//        Utils.attachToy(elementLoader.mot, soundLoader.mot, false, animationLoader.mot, mouthOpenAnimation);
-//
-//        Spinner spinner = new Spinner(soundLoader.open, soundLoader.close);
-//        spinner.init(elementLoader.eyeRightSur, svgElement, 809.87f, 199.86f);
-//
-//        return svgElement;
-//    }
-//
-//    private void initRecorder(OMSVGSVGElement svgElement) {
-//        final LoopRecorder recorder1 = new LoopRecorder();
-//        final LoopRecorder recorder2 = new LoopRecorder();
-//        final LoopRecorderController loopRecorderController = new LoopRecorderController(svgElement);
-//        loopRecorderController.setRecorder(recorder1);
-//        LoopRecorderFactory.setRecorder(recorder1);
-//
-//        OMSVGGElement switcher = Utils.getGElement("switcher", svgElement);
-//        Utils.addHandler(switcher, new Utils.SomeHandler() {
-//            public void handle() {
-//                if(LoopRecorderFactory.getRecorder() == recorder1){
-//                    loopRecorderController.setRecorder(recorder2);
-//                    LoopRecorderFactory.setRecorder(recorder2);
-//                }
-//                else {
-//                    loopRecorderController.setRecorder(recorder1);
-//                    LoopRecorderFactory.setRecorder(recorder1);
-//                }
-//            }
-//        });
-//    }
-//
-//    private boolean isMouthOpen(ElementLoader elementLoader) {
-//        return elementLoader.isMouthOpen();
-//    }
-//
-//    private void createGlassesButton(final SoundLoader soundLoader, final ElementLoader elementLoader, OMSVGGElement teethDown) {
-//        Utils.SomeHandler teethHandler = new Utils.SomeHandler() {
-//            public void handle() {
-//                String display = elementLoader.glasses.getStyle().getDisplay();
-//                if (display.equalsIgnoreCase("none")) {
-//                    soundLoader.toothBottom.play();
-//                    Utils.show(elementLoader.glasses);
-//                }
-//                else {
-//                    Utils.hide(elementLoader.glasses);
-//                }
-//            }
-//        };
-//        Utils.attachToy(teethDown, Utils.getNullSound(), false, new HandlerToyAnimation(teethHandler));
-//    }
+
+    private void bindBlackCube() {
+        Utils.attachToy(elementLoader.blackCube, soundLoader.blackCube, false, animationLoader.blackCube);
+    }
+
+    private OMSVGSVGElement getSvg() {
+        MainBundle mainBundle = GWT.create(MainBundle.class);
+        final SVGResource svgResource = mainBundle.mainSvg();
+        return svgResource.getSvg();
+    }
 }
